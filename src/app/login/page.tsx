29966b741b48ce110c63546388621166a8099538
Evan O'Neil <evanoneil26@gmail.com>
2025-08-14 11:29:28 -0400

    added dummy tagline

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -56,7 +56,7 @@ export default function LoginPage() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#0F0E13] px-4 text-white">
       <div className="mb-10 flex flex-col items-center text-center">
         <img src="https://ynlmvzuedasovzaesjeq.supabase.co/storage/v1/object/public/graphics/theo-logo.svg" alt="THEO Logo" className="h-12 mb-4" />
-        <p className="text-sm text-gray-400 mt-2">The divine right to pick’em</p>
+        <p className="text-sm text-gray-400">More information coming soon</p>
       </div>
 
       <div className="w-full max-w-sm bg-[#1B1A22] p-6 rounded-xl border border-[#2D2C36] shadow">
